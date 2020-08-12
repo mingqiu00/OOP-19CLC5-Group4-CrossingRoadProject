9be689975516d7ece6198d3621d57c95704ff8bb
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2020-08-12 14:56:36 +0700

    Update main.cpp

---
+++ Project/main.cpp
@@ -0,0 +1,31 @@
+#include "CGAME.h"
+int main()
+{
+	CGAME cg;
+	VideoMode vm(960, 540);
+	RenderWindow window(vm, "Cross the road");
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.jpg");
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+	Clock clock;
+	while (window.isOpen())
+	{
+		Time dt = clock.restart();
+		// Press Esc to close
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		cg.updatePosPeople(dt.asSeconds());
+		cg.updatePosVehicle(dt.asSeconds());
+		cg.updatePosAnimal(dt.asSeconds());
+		window.clear();
+		window.draw(spriteBackground);
+		cg.drawGame(window);
+		window.display();
+	}
+
+	return 0;
+}
