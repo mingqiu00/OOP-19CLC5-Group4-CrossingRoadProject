ff007a90925de1a75f2a33c3c0357ec0aa8206b9
Huyen178 <63285465+Huyen178@users.noreply.github.com>
2020-08-21 07:56:05 +0700

    haven't finished yet

--- Project/CVEHICLE.cpp
+++ Project/CVEHICLE.cpp
@@ -25,15 +25,47 @@ void CTRUCK::Move(float elapsedTime)
 	temp.y = this->Pos().y;
 	this->setPos(temp.x, temp.y);
 }
-void CTRUCK::spawn(float x,float y)
+MyRandom::MyRandom()
 {
-	m_Sprite = Sprite(TextureHolder::GetTexture(
-			"graphics/truck.png"));
-		m_Sprite.setPosition(x, y);	
+	srand(time(NULL));
+	ran = rand();
 }
-void CCAR::spawn(float x,float y)
+int MyRandom::next()
 {
-	m_Sprite = Sprite(TextureHolder::GetTexture(
-			"graphics/car.png"));
-		m_Sprite.setPosition(x, y);	
-}	
+	return rand() % RAND_MAX + 0;
+}
+int MyRandom::next(int x)
+{
+	return rand() % x + 0;
+}
+int MyRandom::next(int a, int b)
+{
+	if (a < b)
+		return rand() % (b - a + 1) + a;
+	return rand() % (a - b + 1) + b;
+}
+double MyRandom::nextDouble()
+{
+	return (double)rand() / RAND_MAX;
+}
+bool CTRAFFICLIGHT::getLight()
+{
+	return light;
+}
+void CTRAFFICLIGHT::changeLight()
+{
+	while (1 > 0)
+	{
+		MyRandom r;
+		time = r.nextDouble();
+		if (light == false) light = true;
+		else light = false;
+	}
+}
+void CTRUCK::Stop()
+{
+	sf::Vector2f temp;
+	temp.x = this->Pos().x;
+	temp.y = this->Pos().y;
+	this->setPos(temp.x, temp.y);
+}
