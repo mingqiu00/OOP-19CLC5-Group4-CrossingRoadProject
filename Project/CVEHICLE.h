54fab76baad47aa2c96ffd0c9ea4f5d716d2f3f0
Huyen178 <63285465+Huyen178@users.noreply.github.com>
2020-08-21 07:55:02 +0700

    haven't finished yet

--- Project/CVEHICLE.h
+++ Project/CVEHICLE.h
@@ -8,22 +8,45 @@ class CVEHICLE
 {
 	sf::Sprite m_Sprite;
 public:
-    	virtual sf::Sprite getSprite () {return m_Sprite;}
+	virtual sf::Sprite getSprite() { return m_Sprite; }
 	virtual sf::Vector2f Pos();
 	virtual void Move(float elapsedTime) = 0;
+	virtual void Stop() = 0;
 	virtual void setPos(float a, float b);
 };
 class CCAR : public CVEHICLE
 {
+	friend class TRAFFICLIGHT;
 public:
-	void spawn(float x, float y);
 	void Move(float elapsedTime);
+	void Stop();
 };
 class CTRUCK : public CVEHICLE
 {
+	friend class TRAFFICLIGHT;
 public:
-	void spawn(float x, float y);
 	void Move(float elapsedTime);
+	void Stop();
+};
+class CTRAFFICLIGHT
+{
+	friend class CCAR;
+	friend class CTRUCK; 
+	bool light; //true: green, false: red
+	float time;
+public:
+	bool getLight();
+	void changeLight();
+};
+class MyRandom
+{
+	int ran;
+public:
+	MyRandom();
+	int next();
+	int next(int x);
+	int next(int a, int b);
+	double nextDouble();
 };
 #endif 
 
